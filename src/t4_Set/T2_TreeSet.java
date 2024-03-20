a2e79a25a6a4e22b3c2db2c267309d3d10ecf034
minjiyooh <opozz11@naver.com>
2024-03-20 17:51:52 +0900

    20240320_1751

--- src/t4_Set/T2_TreeSet.java
+++ src/t4_Set/T2_TreeSet.java
@@ -10,12 +10,43 @@ public class T2_TreeSet {
 	public static void main(String[] args) {
 		TreeSet<Integer> treeSet = new TreeSet<Integer>();
 		
-		for(int i=0; i<10; i++) {
-			treeSet.add((int)(Math.random()*100)+1);
+//		for(int i=0; i<10; i++) {
+//			treeSet.add((int)(Math.random()*100)+1);
+//		}
+//		treeSet.add(5);
+//		treeSet.add(5);
+		for(int i=50; i>0; i-=2) {
+			treeSet.add(i);
 		}
-		treeSet.add(5);
 		System.out.println("1.크기 : " + treeSet.size());
 		System.out.println("1.treeSet: " + treeSet);
+		System.out.println();
+		
+		System.out.println("2.first() : " + treeSet.first());
+		System.out.println("3.last() : " + treeSet.last());
+		System.out.println("4.lower() - 검색자료보다 작은것들중 가장 큰값 : " + treeSet.lower(26));
+		System.out.println("5.higher() - 검색자료보다 큰것들중 가장 작은값 : " + treeSet.higher(26));
+		System.out.println("6.floor() - 같거나 작은것 : " + treeSet.floor(25));
+		System.out.println("7.ceiling() - 같거나 큰것 : " + treeSet.ceiling(25));
+		System.out.println();
+		
+		// pollFirst() : 원소들 중에서 작은원소값 순으로 꺼내어준다.
+		System.out.println("8. size() : " + treeSet.size());
+		System.out.print("8. pollFirst : ");
+		for(int i=0; i<25; i++) {
+		  System.out.print(treeSet.pollFirst() + " ");
+		}
+		System.out.println();
+		
+		for(int i=50; i>0; i-=2) {
+			treeSet.add(i);
+		}
+		// pollLast() : 원소들 중에서 큰원소값 순으로 꺼내어준다.
+		System.out.println("9. size() : " + treeSet.size());
+		System.out.print("9. pollLast : ");
+		for(int i=0; i<25; i++) {
+			System.out.print(treeSet.pollLast() + " ");
+		}
 		System.out.println();
 	}
 }
